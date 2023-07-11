ef9346cca98df828e2c0818c72c3abbbf924412d
Karibasaveshwara_TG <karibasaveshwara.tg@moonraft.com>
2023-07-11 16:13:30 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'test1';
+  otp: string = '';
+
+  onSubmit() {
+    if ('OTPCredential' in window) {
+      const ac = new AbortController();
+      const form = document.querySelector('form');
+      if (form) {
+        form.addEventListener('submit', (e) => {
+          ac.abort();
+        });
+      }
+      const something: any = navigator.credentials;
+      something
+        .get({
+          otp: { transport: ['sms'] },
+          signal: ac.signal,
+        })
+        .then((otp: any) => {
+          this.otp = otp.code;
+          if (form) form.submit();
+        })
+        .catch((err: any) => {
+          console.log(err);
+        });
+    }
+  }
 }
